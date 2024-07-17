4c7a519ad52355dd3f6d8767089ca1e568fe76c3
zeynep-kirencigil <zeynepkirencigil@gmail.com>
2024-07-17 14:48:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,6 @@ def predict():
         return jsonify({'message': 'No case found.'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
